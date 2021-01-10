590d1a680f7e2fe54b228f6388a5bc9bb15d39be
Heliferepo <mattisdalleau@gmail.com>
2021-01-10 17:45:02 +0400

    fixed estrcmp

--- lib/estring/cmp/estrncmp.c
+++ lib/estring/cmp/estrncmp.c
@@ -8,20 +8,11 @@
 #include <estring.h>
 #include <estdlib.h>
 
-static int get_lowest_estrncmp(char const *s1, char const *s2, size_t min)
-{
-    return ((s1[min] > s2[min]) ? 1 : -1);
-}
-
 int estrncmp(char const *s1, char const *s2, size_t n)
 {
-    size_t sizes1 = estrlen(s1);
-    size_t sizes2 = estrlen(s2);
-
-    if (sizes1 < n || sizes2 < n) {
-        if (sizes1 != sizes2)
-            return (get_lowest_estrncmp(s1, s2, MIN(sizes1, sizes2)));
-        return (ememcmp(s1, s2, sizes1));
+    for (size_t i = 0; (s1[i] || s2[i]) && i < n; i++) {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
     }
-    return (ememcmp(s1, s2, n));
+    return (0);
 }
