153fab9a107b5cad3ca379f75c04fb2b71055db6
Heliferepo <mattisdalleau@gmail.com>
2021-01-10 17:46:29 +0400

    fixed ememcmp and ememcpy missing last byte

--- lib/estring/mem/ememcmp.c
+++ lib/estring/mem/ememcmp.c
@@ -12,9 +12,12 @@ int ememcmp(void const *s1, void const *s2, size_t n)
     unsigned char *s1_s = (unsigned char *)s1;
     unsigned char *s2_s = (unsigned char *)s2;
 
-    while (n--)
+    while (n--) {
         if (*s1_s != *s2_s)
             return ((*s1_s > *s2_s) ? 1 : -1);
+        s1_s++;
+        s2_s++;
+    }
     if (*s1_s != *s2_s)
         return ((*s1_s > *s2_s) ? 1 : -1);
     return (0);
--- lib/estring/mem/ememcpy.c
+++ lib/estring/mem/ememcpy.c
@@ -7,7 +7,7 @@
 
 #include <ectypes.h>
 
-void *ememcpy(void *dest, void *src, size_t size)
+void *ememcpy(void *dest, void const *src, size_t size)
 {
     unsigned char *dest_s = (unsigned char *)dest;
     unsigned char *src_s = (unsigned char *)src;
