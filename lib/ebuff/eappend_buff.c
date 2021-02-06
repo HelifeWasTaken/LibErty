81bdf327bf859b13bece7e69127614b5d1de08f8
Mattis DALLEAU <mattisdalleau@gmail.com>
2021-02-06 12:07:40 +0400

    fix nested include lighter hashmap fix norm

--- lib/ebuff/eappend_buff.c
+++ lib/ebuff/eappend_buff.c
@@ -34,7 +34,6 @@ ssize_t eappend_buff_char(ebuff_t **buff_info, i32_t c)
     return (eappend_buff_nbytes(buff_info, (cstr_t)&c, 1));
 }
 
-
 ssize_t eappend_buff_unsigned_number(ebuff_t **buff,
         u64_t nb, char const *base, u8_t base_size)
 {
