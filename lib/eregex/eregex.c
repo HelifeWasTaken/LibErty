4d5446b9b18c914f19b5e171045da2eb76ecff85
Heliferepo <mattisdalleau@gmail.com>
2021-03-13 13:35:27 +0400

    added question mark to regex

--- lib/eregex/eregex.c
+++ lib/eregex/eregex.c
@@ -8,39 +8,45 @@
 #include <erty/eregex.h>
 #include <erty/ectypes.h>
 
-static int match_it(char *expr, char const *regexpr, char **endptr);
+static int match_it(char *expr, char const *reg, char **endptr);
 
-static int match_prev(char const prev, char *expr,
-        char const *regexpr, char **endptr)
+static int match_question_mark(char *expr, char const *reg, char **endptr)
+{
+    if (*reg == *expr && match_it(reg + 2, expr + 1, endptr))
+        return (1);
+    else
+        return (match_it(reg + 2, expr, endptr));
+}
+
+static int match_prev_star(char const prev, char *expr,
+        char const *reg, char **endptr)
 {
     do {
-        if (match_it(expr, regexpr, endptr))
+        if (match_it(expr, reg, endptr))
             return (1);
     } while (*expr && (*expr++ == prev || prev == '.'));
     return (0);
 }
 
-static int match_it(char *expr, char const *regexpr, char **endptr)
+static int match_it(char *expr, char const *reg, char **endptr)
 {
-    if (*regexpr == '\0') {
+    if (*reg == '\0' || (*reg == '$' && *(reg + 1) == '\0' && *expr == '\0')) {
         *endptr = expr;
         return (1);
     }
-    if (*(regexpr + 1) == '*')
-        return (match_prev(*regexpr, expr, regexpr + 2, endptr));
-    if (*regexpr == '$' && *(regexpr + 1) == '\0' && *expr == '\0') {
-        *endptr = expr;
-        return (1);
-    }
-    if ((*expr && *regexpr == '.') || (*expr && *regexpr == *expr))
-        return (match_it(expr + 1, regexpr + 1, endptr));
+    if (*(reg + 1) == '?')
+        return (match_question_mark(expr, reg, endptr));
+    if (*(reg + 1) == '*')
+        return (match_prev_star(*reg, expr, reg + 2, endptr));
+    if ((*expr && *reg == '.') || (*expr && *reg == *expr))
+        return (match_it(expr + 1, reg + 1, endptr));
     return (0);
 }
 
-int regex(char *expr, char const *regexpr, char **startptr, char **endptr)
+int regex(char *expr, char const *reg, char **startptr, char **endptr)
 {
     do {
-        if (match_it(expr, regexpr, endptr)) {
+        if (match_it(expr, reg, endptr)) {
             *startptr = expr;
             return (1);
         }
