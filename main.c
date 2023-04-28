0fd280bb7995fc6344fb5dda518ee07943d304f7
Mattis DALLEAU <mattis.dalleau@epitech.eu>
2023-04-28 13:42:52 +0400

    Update main.c

--- main.c
+++ main.c
@@ -6,31 +6,22 @@
 #include <erty/json.h>
 #include <erty/elinked.h>
 #include <erty/evector.h>
+#include <erty/eprintf.h>
 
 int main(int ac, char **av)
 {
     struct json conf = {0};
-    char *file = NULL;
 
     if (ac != 2) {
-        ASSERT("LibSeraph", "defaults to test2.json");
-        file = "./test2.json";
-    }else
-        file = av[1];
-    if (json_parser(&conf, file) == false)
-        return (132);
-    struct json_hashmap *map = conf.v.object;
+        efprintf(stderr, "Please give a file in input\n");
+        return (1);
+    }
 
-    struct json_array *arr = map->get(&map, "properties").value.v.object;
-    map = arr->data[1].v.object;
-    printf("%s\n", map->get(&map, "name").value.v.string);
-    /*
-    struct json_array *arr = map->get(&map, "properties").value.v.array;
-    printf("%ld\n", arr->size);
-    fflush(stdout);
-    printf("%s\n", map->get(&map, "value").value.v.string);
-//    printf("%s\n", map->get(&map, "value").value.v.string);
-    */
+    if (json_parser(&conf, av[1]) == false) {
+        efprintf(stderr, "Invalid json parsed");
+        return (132);
+    }
+    eprintf("Your File is a valid Json file\n");
     destroy_json(&conf);
     eprintf_free_buff();
     return 0;
